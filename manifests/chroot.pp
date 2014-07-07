87063e401e7db2e8e8b65946c912c061fe96bf85
Adam Breaux <breauxaj@gmail.com>
2014-07-07 14:31:02 -0400

    added docs

--- manifests/chroot.pp
+++ manifests/chroot.pp
@@ -1,3 +1,32 @@
+# Class: bind::chroot
+#
+# This class installs Bind chroot package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the bind chroot package
+#
+# Actions:
+#   - Install Bind chroot package
+#   - Install Bind package as dependency
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'bind::chroot':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'bind::chroot':
+#      ensure => 'absent'
+#    }
+#
 class bind::chroot (
   $ensure = 'latest'
 ){
