bbf366d5892295e161945d9fb74b97ff040d8da2
Adam Breaux <breauxaj@gmail.com>
2014-07-07 14:26:26 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: bind
+#
+# This class installs Bind name server
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the balance package
+#
+# Actions:
+#   - Install Bind
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'bind':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'bind':
+#      ensure => 'absent'
+#    }
+#
 class bind (
   $ensure = 'latest'
 ){
